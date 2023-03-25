d55c0cb204ee4ee1adf1dab770ecdb055629625f
aaron <20100312@mail.wit.ie>
2023-03-25 22:55:39 +0000

    style: Enhance GenreList styling and adapt to dark mode

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,20 @@
-import { Box, Divider, Heading, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Heading,
+  List,
+  ListItem,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
   const { genres, error, isLoading } = useGenres();
+  const { colorMode } = useColorMode();
+  const bgColor = { light: "white", dark: "gray.700" };
+  const borderColor = { light: "gray.200", dark: "gray.600" };
+  const hoverColor = { light: "gray.200", dark: "gray.500" };
 
   if (isLoading) {
     return <Text>Loading genres...</Text>;
@@ -13,14 +25,31 @@ const GenreList = () => {
   }
 
   return (
-    <Box>
-      <Heading as="h3" size="md" mb="4">
+    <Box
+      boxShadow="md"
+      borderWidth="1px"
+      borderRadius="md"
+      borderColor={borderColor[colorMode]}
+      p="4"
+      bgColor={bgColor[colorMode]}
+    >
+      <Heading as="h3" size="md" mb="4" textAlign="center">
         Genres
       </Heading>
       <Divider mb="4" />
-      <List spacing={2}>
+      <List spacing={3}>
         {genres.map((genre) => (
-          <ListItem key={genre.id} fontSize="sm">
+          <ListItem
+            key={genre.id}
+            fontSize="sm"
+            px="3"
+            py="2"
+            borderRadius="md"
+            fontWeight="medium"
+            cursor="pointer"
+            transition="all 0.2s"
+            _hover={{ bgColor: hoverColor[colorMode] }}
+          >
             {genre.name}
           </ListItem>
         ))}
