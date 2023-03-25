ee0c5603a7b95cd7c77e8a8e970c20c531bf60a6
aaron <20100312@mail.wit.ie>
2023-03-25 12:13:53 +0000

    feat: Add useMovies custom hook with request cancellation support

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,27 +1,8 @@
 import { Text } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Movie {
-  id: number;
-  original_title: string;
-}
-
-interface FetchMoviesResponse {
-  count: number;
-  results: Movie[];
-}
+import useMovies from "../hooks/useMovies";
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchMoviesResponse>("/movie/popular")
-      .then((res) => setMovies(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []); // Add an empty dependency array
+  const { movies, error } = useMovies();
 
   return (
     <>
---
+++ src/hooks/useMovies.ts
@@ -0,0 +1,34 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Movie {
+    id: number;
+    original_title: string;
+  }
+  
+  interface FetchMoviesResponse {
+    count: number;
+    results: Movie[];
+  }
+
+const useMovies = () =>{
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+      const controller = new AbortController();
+      apiClient
+        .get<FetchMoviesResponse>("/movie/popular", {signal : controller.signal})
+        .then((res) => setMovies(res.data.results))
+        .catch((err) => {
+            if (err instanceof CanceledError) return ;
+            setError(err.message)
+        })
+        return () => controller.abort()
+    }, []);
+
+    return {movies, error}
+}
+
+export default useMovies
