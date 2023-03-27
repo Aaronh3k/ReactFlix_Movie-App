2dca7a61718a2cc74826997aabc3f9e5bc74a88f
aaron <20100312@mail.wit.ie>
2023-03-27 23:18:02 +0100

    feat:Add Cast to Movie Details Page

--- src/components/MovieDetailsPage.tsx
+++ src/components/MovieDetailsPage.tsx
@@ -7,12 +7,13 @@ import {
   HStack,
   Tag,
   Flex,
+  SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
 import useMovieDetails from "../hooks/useMovieDetails";
 import apiClient from "../services/api-client";
 import ScrollableImage from "../components/ScrollableImage";
-
+import useMovieCredits, { Cast } from "../hooks/useMovieCredits";
 export interface MovieDetails {
   original_title: string;
   overview: string;
@@ -28,16 +29,51 @@ const MovieDetailsPage = () => {
   const params = useParams<{ movieId?: string }>();
   const movieId = params.movieId ? parseInt(params.movieId) : undefined;
   const { movieDetails, error, isLoading } = useMovieDetails(movieId);
+  const {
+    cast,
+    error: castError,
+    isLoading: castLoading,
+  } = useMovieCredits(movieId);
   const imageUrl = apiClient.baseImageUrl;
   const boxShadowColor = useColorModeValue("gray.400", "gray.800");
 
-  if (isLoading) {
+  if (isLoading || castLoading) {
     return <Text>Loading...</Text>;
   }
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
+  if (error || castError) {
+    return <Text>Error: {error || castError}</Text>;
   }
+
+  const renderCast = (castMembers: Cast[]) => {
+    return (
+      <VStack spacing={4} align="start" mt={6}>
+        <Text fontSize="2xl" fontWeight="bold">
+          Cast
+        </Text>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={4}>
+          {castMembers.slice(0, 10).map((member) => (
+            <HStack key={member.id} spacing={4}>
+              {member.profile_path && (
+                <Image
+                  src={`${imageUrl}w92${member.profile_path}`}
+                  alt={member.name}
+                  borderRadius="md"
+                  boxShadow={`0 4px 6px ${boxShadowColor}`}
+                />
+              )}
+              <VStack align="start" spacing={0}>
+                <Text fontWeight="bold">{member.name}</Text>
+                <Text fontSize="sm" color="gray.500">
+                  {member.character}
+                </Text>
+              </VStack>
+            </HStack>
+          ))}
+        </SimpleGrid>
+      </VStack>
+    );
+  };
 
   return (
     <Box>
@@ -51,13 +87,16 @@ const MovieDetailsPage = () => {
         mx={{ base: 4, md: 16 }}
       >
         <Image
-          src={`${imageUrl}w500${movieDetails?.poster_path}`}
+          src={`${imageUrl}original${movieDetails?.poster_path}`}
           alt={movieDetails?.original_title}
-          width={{ base: "100%", md: "30%" }}
+          width="400px" // Set the fixed width
+          height="800px" // Set the fixed height
+          objectFit="cover" // Ensure the image is resized correctly
           borderRadius="md"
           boxShadow={`0 4px 6px ${boxShadowColor}`}
           mb={{ base: 4, md: 0 }}
         />
+
         <VStack align="start" spacing={4} ml={{ base: 0, md: 8 }} flex="1">
           <Text fontSize="3xl" fontWeight="bold">
             {movieDetails?.original_title}
@@ -76,6 +115,7 @@ const MovieDetailsPage = () => {
             <Text fontWeight="bold">Overview:</Text>
             <Text>{movieDetails?.overview}</Text>
           </Box>
+          {cast && renderCast(cast)}
         </VStack>
       </Flex>
     </Box>
---
+++ src/hooks/useMovieCredits.ts
@@ -0,0 +1,45 @@
+// hooks/useMovieCredits.ts
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+
+export interface Cast {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+}
+
+const useMovieCredits = (movieId: number | undefined) => {
+  const [cast, setCast] = useState<Cast[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (typeof movieId === "undefined") {
+      setError("Invalid movie ID");
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchMovieCredits = async () => {
+      try {
+        const response = await apiClient.get<{ cast: Cast[] }>(
+          `/movie/${movieId}/credits`
+        );
+        setCast(response.data.cast);
+        setIsLoading(false);
+      } catch (err: any) {
+        setError(
+          err.message || "An error occurred while fetching movie credits"
+        );
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovieCredits();
+  }, [movieId]);
+
+  return { cast, error, isLoading };
+};
+
+export default useMovieCredits;
