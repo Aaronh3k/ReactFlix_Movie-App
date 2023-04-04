ef47b5a7b6e350b98a4a493a7db31f381f9e7980
aaron <20100312@mail.wit.ie>
2023-04-04 12:26:17 +0100

    feat: Enhance homepage with stylish buttons and improved visibility

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,32 +1,63 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  VStack,
+  HStack,
+  Link,
+  Button,
+} from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
-    <Box>
-      <h1>Welcome to ReactFlix!</h1>
-      <p>
-        ReactFlix is your ultimate destination for movies and TV shows. Browse
-        through our amazing collection of movies and TV shows, check out
-        trending content, and add your favorites to your personal list.
-      </p>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/movies">Movies</a>
-        </li>
-        <li>
-          <a href="/tvshows">TV Shows</a>
-        </li>
-        <li>
-          <a href="/trending">Trending</a>
-        </li>
-        <li>
-          <a href="/favorites">Favorites</a>
-        </li>
-      </ul>
+    <Box
+      w="100vw"
+      h="100vh"
+      bgImage="url('https://source.unsplash.com/random?entertainment')"
+      bgSize="cover"
+      bgPosition="center"
+      alignItems="center"
+      justifyContent="center"
+      display="flex"
+      flexDirection="column"
+    >
+      <VStack
+        spacing={5}
+        textAlign="center"
+        bg="rgba(0, 0, 0, 0.6)"
+        p={6}
+        borderRadius="md"
+      >
+        <Heading color="white" fontSize="4xl">
+          Welcome to ReactFlix!
+        </Heading>
+        <Text color="white" fontSize="xl">
+          ReactFlix is your ultimate destination for movies and TV shows. Browse
+          through our amazing collection of movies and TV shows, check out
+          trending content, and add your favorites to your personal list.
+        </Text>
+        <HStack spacing={4}>
+          {[
+            { href: "/movies", label: "Movies" },
+            { href: "/tvshows", label: "TV Shows" },
+            { href: "/trending", label: "Trending" },
+            { href: "/favorites", label: "Favorites" },
+          ].map(({ href, label }) => (
+            <Link href={href} key={href} textDecoration="none">
+              <Button
+                bg="rgba(30, 144, 255, 0.8)"
+                _hover={{ bg: "rgba(30, 144, 255, 1)" }}
+                color="white"
+                fontWeight="bold"
+                borderRadius="md"
+              >
+                {label}
+              </Button>
+            </Link>
+          ))}
+        </HStack>
+      </VStack>
     </Box>
   );
 };
