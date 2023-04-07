95b9de62f69cced7db6f84804e2b07796bc023e6
aaron <20100312@mail.wit.ie>
2023-04-07 15:39:14 +0100

    feat: Add hover effect and navigation to Person Deatail Page in Person Card

--- src/components/PersonCard.tsx
+++ src/components/PersonCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Image, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import apiClient from "../services/api-client";
 
 interface Props {
@@ -9,20 +10,33 @@ const PersonCard = ({ person }: Props) => {
   const imageUrl = apiClient.baseImageUrl + "w342/" + person.profile_path;
 
   return (
-    <Box
-      userSelect="none"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-    >
-      <Image src={imageUrl} alt={person.name} />
-      <VStack p="6" spacing="1">
-        <Text fontWeight="bold" fontSize="xl">
-          {person.name}
-        </Text>
-        <Text>Popularity: {person.popularity.toFixed(1)}</Text>
-      </VStack>
-    </Box>
+    <Link to={`/person/${person.id}`}>
+      <Box
+        userSelect="none"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        transition="all 0.2s"
+        _hover={{
+          textDecoration: "none",
+          boxShadow: "xl",
+          transform: "scale(1.05)",
+        }}
+        role="group"
+      >
+        <Image src={imageUrl} alt={person.name} />
+        <VStack p="6" spacing="1">
+          <Text
+            fontWeight="bold"
+            fontSize="xl"
+            _groupHover={{ textDecoration: "underline" }}
+          >
+            {person.name}
+          </Text>
+          <Text>Popularity: {person.popularity.toFixed(1)}</Text>
+        </VStack>
+      </Box>
+    </Link>
   );
 };
 
