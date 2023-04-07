d951ea5a10090f5928786340dca7ce4646162d42
aaron <20100312@mail.wit.ie>
2023-04-07 21:27:16 +0100

    feat: Add Person Credits in Person Details Page

--- src/components/PersonDetailsPage.tsx
+++ src/components/PersonDetailsPage.tsx
@@ -1,29 +1,77 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import {
   Box,
   Text,
   Image,
   VStack,
   HStack,
+  SimpleGrid,
   useColorModeValue,
+  Center,
 } from "@chakra-ui/react";
+
 import usePersonDetails from "../hooks/usePersonDetails";
+import usePersonCredits from "../hooks/usePersonCredits";
 import apiClient from "../services/api-client";
+import DefaultProfileImage from "./DefaultProfileImage";
 
 const PersonDetailsPage = () => {
-  const params = useParams<{ personId?: string }>();
-  const personId = params.personId ? parseInt(params.personId) : undefined;
+  const { personId: personIdParam } = useParams<{ personId?: string }>();
+  const personId = personIdParam ? parseInt(personIdParam) : undefined;
   const { personDetails, error, isLoading } = usePersonDetails(personId);
+  const {
+    movies,
+    tvShows,
+    error: creditsError,
+    isLoading: creditsLoading,
+  } = usePersonCredits(personId);
   const imageUrl = apiClient.baseImageUrl;
   const boxShadowColor = useColorModeValue("gray.400", "gray.800");
 
-  if (isLoading) {
+  if (isLoading || creditsLoading) {
     return <Text>Loading...</Text>;
   }
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
+  if (error || creditsError) {
+    return <Text>Error: {error || creditsError}</Text>;
   }
+
+  const renderCredits = (title: string, items: any[], type: string) => {
+    return (
+      <VStack spacing={4} align="start" mt={6} w="100%">
+        <Text fontSize="2xl" fontWeight="bold">
+          {title}
+        </Text>
+        <SimpleGrid
+          columns={{ base: 2, sm: 3, md: 4, lg: 6 }}
+          spacing={4}
+          w="100%"
+        >
+          {items.map((item) => (
+            <Center key={item.id}>
+              <Link to={`/${type}/${item.id}`}>
+                <VStack align="center" spacing={2}>
+                  {item.poster_path ? (
+                    <Image
+                      src={`${imageUrl}w92${item.poster_path}`}
+                      alt={item.title || item.name}
+                      borderRadius="md"
+                      boxShadow={`0 4px 6px ${boxShadowColor}`}
+                    />
+                  ) : (
+                    <DefaultProfileImage />
+                  )}
+                  <Text fontWeight="bold" textAlign="center">
+                    {item.title || item.name}
+                  </Text>
+                </VStack>
+              </Link>
+            </Center>
+          ))}
+        </SimpleGrid>
+      </VStack>
+    );
+  };
 
   return (
     <Box userSelect="none">
@@ -54,6 +102,8 @@ const PersonDetailsPage = () => {
             <Text fontWeight="bold">Biography:</Text>
             <Text>{personDetails?.biography}</Text>
           </Box>
+          {movies && renderCredits("Movies:", movies, "movie")}
+          {tvShows && renderCredits("TV Shows:", tvShows, "tv")}
         </VStack>
       </HStack>
     </Box>
---
+++ src/hooks/usePersonCredits.ts
@@ -0,0 +1,48 @@
+// src/hooks/usePersonCredits.ts
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+type MovieCredit = {
+  id: number;
+  title: string;
+  poster_path: string;
+};
+
+type TVShowCredit = {
+  id: number;
+  name: string;
+  poster_path: string;
+};
+
+const usePersonCredits = (personId: number | undefined) => {
+  const [movies, setMovies] = useState<MovieCredit[]>([]);
+  const [tvShows, setTVShows] = useState<TVShowCredit[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!personId) return;
+
+    const fetchCredits = async () => {
+      setLoading(true);
+      try {
+        const movieCredits = await apiClient.get(
+          `person/${personId}/movie_credits`
+        );
+        const tvCredits = await apiClient.get(`person/${personId}/tv_credits`);
+        setMovies(movieCredits.data.cast);
+        setTVShows(tvCredits.data.cast);
+        setLoading(false);
+      } catch (err: any) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+
+    fetchCredits();
+  }, [personId]);
+
+  return { movies, tvShows, error, isLoading };
+};
+
+export default usePersonCredits;
